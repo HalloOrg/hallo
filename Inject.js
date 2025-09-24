7a705621d382ff5de239fdddc8d55952189c97ed
HalloOrg <webhackjohnttt@outlook.com>
2025-09-23 23:26:21 -0400

    Update Inject.js

--- Inject.js
+++ Inject.js
@@ -1,4 +1,4 @@
---Bookmark this on your school account for it to be used easier instead of dooing the whole process lol
+//Bookmark this on your school pc for easier way to inject, by copying the code and putting it in there for url
 
 
 (async () => {
